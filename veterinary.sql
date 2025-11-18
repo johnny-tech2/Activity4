87e164e44344c21275e5ffc96411276b612e1280
Dwight-buno <24-32880@g.batstate-u.edu.ph>
2025-11-18 13:08:45 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -1,10 +1,24 @@
 -- veterinary.sql file
-CREATE TABLE Pets (
-    PetID INT PRIMARY KEY,
-    Name VARCHAR(50),
-    Species VARCHAR(50),
-    Breed VARCHAR(50),
-    Age INT,
-    OwnerID INT,
-    FOREIGN KEY (OwnerID) REFERENCES Owners(OwnerID)
+CREATE DATABASE veterinary,
+
+CREATE TABLE owners(
+    ownerid INT PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
 );
+
+CREATE TABLE animals (
+    animalid INT PRIMARY KEY,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    dateofbirth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+    ownerid INT,
+    FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
+ 
