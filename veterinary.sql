b2098ee8320ab53264d1834dfc9f97bb63177a63
johnny-tech2 <johngilbertorias@gmail.com>
2025-11-18 12:54:50 +0800

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,2 +1,10 @@
 -- veterinary.sql file
-CREATE DATABASE veterinary,
+CREATE TABLE Pets (
+    PetID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Species VARCHAR(50),
+    Breed VARCHAR(50),
+    Age INT,
+    OwnerID INT,
+    FOREIGN KEY (OwnerID) REFERENCES Owners(OwnerID)
+);
