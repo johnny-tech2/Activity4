5f1f66cb09b4ec9c6effdb848c2c54e2d31f0170
johnny-tech2 <johngilbertorias@gmail.com>
2025-11-18 15:58:48 +0800

    feat/list-species-catered

--- veterinary.sql
+++ veterinary.sql
@@ -132,3 +132,4 @@ ALTER TABLE owners ADD COLUMN registereddate DATE;
 ALTER TABLE invoices RENAME COLUMN paymentdate TO paymenttime;
 DELETE FROM appointments WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba');
 UPDATE doctors SET dlastname = 'Reyes-Gonzales' WHERE dfirstname = 'Sofia' AND dlastname = 'Reyes';
+SELECT DISTINCT species FROM animals; Expected Output: Dog, Cat
